6c4dce66e1d2f0e189550009927347e7ae3e2014
vagrant <vagrant@localhost.localdomain>
2020-06-09 12:58:10 +0000

    [Fix] 5-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,6 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    # binding.pry
     if @book.save
        flash[:notice] = "You have creatad book successfully."
        redirect_to book_path(@book.id)
@@ -25,11 +24,15 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    if @book.update(book_params)
-      flash[:notice] = "You have updated book successfully."
-      redirect_to book_path(@book.id)
+    if @book.user_id != current_user.id
+      redirect_to books_path
     else
-      render :edit
+      if @book.update(book_params)
+        flash[:notice] = "You have updated book successfully."
+        redirect_to book_path(@book.id)
+      else
+        render :edit
+      end
     end
   end
 
@@ -42,14 +45,17 @@ class BooksController < ApplicationController
 
   def destroy
     @book = Book.find(params[:id])
-    @book.destroy
-    redirect_to books_path
+    if @book.user_id != current_user.id
+      redirect_to books_path
+    else
+      @book.destroy
+      redirect_to books_path
+    end
   end
 
   def show
     @book = Book.find(params[:id])
-    @user = current_user
-    # binding.pry
+    @user = @book.user
   end
 
   private
